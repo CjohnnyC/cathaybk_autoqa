7c50a5d64cd538e73530f52d8351d29d6a861a8f
CjohnnyC <116575428+CjohnnyC@users.noreply.github.com>
2022-11-15 13:58:29 +0800

    page element
    page element

--- autotest/pageElement.py
+++ autotest/pageElement.py
@@ -31,4 +31,12 @@ class PageElement:
         return self.driver.find_element(By.XPATH, "//p[text()='停發卡']")
 
     def dead_card_group(self):
-        return self.driver.find_elements(By.XPATH, "//div[contains(text(),'(停發)')]")
+        dcg=[]
+        dcg.append(self.driver.find_element(By.XPATH, "//img[@id='img_8761B214795949419DC51176802BF6E1']"))
+        dcg.append(self.driver.find_element(By.XPATH, "//img[@id='img_9B6112FD24B948C99668101C08F8B675']"))
+        dcg.append(self.driver.find_element(By.XPATH, "//img[@id='img_92C805AD70BA4B5EAD22B41FDD858B1D']"))
+        dcg.append(self.driver.find_element(By.XPATH, "//img[@id='img_CAD5544A547F429CA7414EFE3814F85E']"))
+        dcg.append(self.driver.find_element(By.XPATH, "//img[@id='img_AA5E3E225F74485EBAC0F16AEC06C072']"))
+        dcg.append(self.driver.find_element(By.XPATH, "//img[@id='img_3B4E85E2B5224CFCBE78AE5D9250481D']"))
+        dcg.append(self.driver.find_element(By.XPATH, "//img[@id='img_76FB9CA0074844B8B2871BF9BDD09DFB']"))
+        return dcg
