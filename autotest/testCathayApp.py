ad37c2d3e6148a87691ae23fb861e7249c8fe1cf
CjohnnyC <116575428+CjohnnyC@users.noreply.github.com>
2022-11-18 21:45:47 +0800

    Update testCathayApp.py

--- autotest/testCathayApp.py
+++ autotest/testCathayApp.py
@@ -1,8 +1,8 @@
 import pytest
 from appium import webdriver
-from appium.webdriver.common.touch_action import TouchAction
-from pathlib import Path
+from datetime import datetime
 from time import sleep
+#from appium.webdriver.common.touch_action import TouchAction
 
 #Self defined configuration and page object
 from appConfig import CAPS
@@ -18,7 +18,7 @@ class TestCathayApp:
         cls.driver=webdriver.Remote("http://localhost:4723/wd/hub", CAPS)
         cls.driver.implicitly_wait(10) #We can also use WebDriverWait to each element, or set sleep.
         cls.pe=PageElement(cls.driver)
-        cls.actions=TouchAction(cls.driver)
+        #cls.actions=TouchAction(cls.driver)
 
     @classmethod
     def teardown_class(cls): 
@@ -33,7 +33,9 @@ class TestCathayApp:
     def test1_homepage(self):
         self.driver.get("https://www.cathaybk.com.tw/cathaybk/")
         self.getPage=is_exist(self.pe.login()) #other page elements AND logic should be True
-        screenshot(self.driver)
+        NTIME=datetime.now().strftime("%Y%m%d_%H%M%S")
+        homepage=file_path("./screenshot/homepage", "homepage_"+NTIME+".png")
+        self.driver.get_screenshot_as_file(homepage)
         assert self.getPage is True #Confirm homepage displayed
 
     def test2_credit_card_list(self):
@@ -41,29 +43,35 @@ class TestCathayApp:
         self.pe.prod_intro().click()
         self.pe.credit_card().click()
         ccl=self.pe.credit_card_list()
-        scroll_el1_to_el2(self.actions, ccl[0], ccl[7]) #scroll from card intro to apply credit card
-        screenshot(self.driver)
+        self.driver.drag_and_drop(ccl[0], ccl[7])
+        #scroll_el1_to_el2(self.actions, ccl[0], ccl[7]) #TochAction not work on H5 page
+        NTIME=datetime.now().strftime("%Y%m%d_%H%M%S")
+        cardlist=file_path("./screenshot/cardlist", "cardlist_"+NTIME+".png")
+        self.driver.get_screenshot_as_file(cardlist)
         assert len(ccl)==8 #Confirm credit card list has 8 subfunctions
 
     def test3_deadcard_type(self):
         ccl=self.pe.credit_card_list()
-        scroll_el1_to_el2(self.actions, ccl[7], ccl[0]) #scroll from apply credit card back to card intro
+        self.driver.drag_and_drop(ccl[7], ccl[0])
         ccl[0].click() #click card intro
-        scroll_el1_to_el2(self.actions, self.pe.recmd_card(), self.pe.dead_card())
+        self.driver.drag_and_drop(self.pe.recmd_card(), self.pe.dead_card())
         self.pe.dead_card().click()
         screenshot_count=0
         dcg=self.pe.dead_card_group()
         for i in range(len(dcg)):
             sleep(3)
-            screenshot(self.driver)
+            NTIME=datetime.now().strftime("%Y%m%d_%H%M%S")
+            cardgroup=file_path("./screenshot/cardgroup", "cardgroup_"+NTIME+".png")
+            self.driver.get_screenshot_as_file(cardgroup)
             screenshot_count+=1
             if i<len(dcg)-1:
-                scroll_el1_to_el2(self.actions, dcg[i], dcg[i+1]) #TouchAction scroll to next card
+                self.driver.drag_and_drop(dcg[i], dcg[i+1])
             else:
                 break
         assert screenshot_count==len(dcg) #Confirm screenshot num equal to dead card num
 
 if __name__=="__main__":
-    html_report=str(Path.cwd()/Path("report/result_"+NTIME+".html"))
+    NTIME=datetime.now().strftime("%Y%m%d_%H%M%S")
+    html_report=file_path("./report", "result_"+NTIME+".html")
     pytest.main(["-s", "-v", ".", "--html="+html_report])
     #export html report
