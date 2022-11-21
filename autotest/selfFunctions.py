b817ad9e784e3f8f598fa3ef8ddc4fd12b8bca44
CjohnnyC <116575428+CjohnnyC@users.noreply.github.com>
2022-11-21 18:13:29 +0800

    Update selfFunctions.py
    Update generic functions

--- autotest/selfFunctions.py
+++ autotest/selfFunctions.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
+TIME_FORMAT="%Y%m%d_%H%M%S"
+'''
 #NTIME=datetime.now().strftime("%Y%m%d_%H%M%S") 
 #Above NTIME need to call each time, can not package it or the time will be the same
-TIME_FORMAT="%Y%m%d_%H%M%S"
+'''
 
 def is_exist(page_element):
     flag=True
@@ -13,6 +15,26 @@ def is_exist(page_element):
         flag=False
         return flag
 
+def js_vertical_scroll_top(driver, elements_list):
+    count=0
+    while True:
+        try:
+            driver.execute_script("arguments[0].scrollIntoView(true)", eval(elements_list[count]))
+            count+=1
+        except:
+            break
+    return count
+
+def horizontal_scroll_each(driver, elements_list):
+    count=0
+    while True:
+        try:
+            driver.sroll(eval(elements_list[count+1]), eval(elements_list[count]))
+            count+=1
+        except:
+            break
+    return count
+
 def file_path(dir, file):
     Path(dir).mkdir(parents=True, exist_ok=True) 
     wholepath=Path(dir)/Path(file)
@@ -21,33 +43,26 @@ def file_path(dir, file):
 def screenshot(driver, dir, file):
     driver.get_screenshot_as_file(file_path(dir, file))
 
-def swipe_card(driver, card_x, card_width, card_y):
-    scroll_x=card_x+card_width
-    driver.swipe(scroll_x, card_y, 0, card_y)
-
-
 '''
 def scroll_el1_to_el2(actions, el1, el2):
     actions.press(el1).move_to(el2).release().perform()
 '''
 
-'''
 def get_size(driver):
     x=driver.get_window_size()["width"]
     y=driver.get_window_size()['height']
     return (x,y)
 
-def swipe_up_down(driver,x,y1,y2,t):
+def swipe_up_down(driver,x_ratio,sy_ratio,ey_ratio,t):
     s=get_size(driver)
-    x=int(s[0]*x)
-    y1=int(s[1]*y1)
-    y2=int(s[1]*y2)
-    driver.swipe(x,y1,x,y2,t)
+    x=int(s[0]*x_ratio)
+    sy=int(s[1]*sy_ratio)
+    ey=int(s[1]*ey_ratio)
+    driver.swipe(x,sy,x,ey,t)
 
-def swipe_left_right(driver,x1,x2,y,t):
+def swipe_left_right(driver,sx_ratio,ex_ratio,y_ratio,t):
     s=get_size(driver)
-    x1=int(s[0]*x1)
-    x2=int(s[0]*x2)
-    y=int(s[1]*y)
-    driver.swipe(x1,y,x2,y,t)
-'''
+    sx=int(s[0]*sx_ratio)
+    ex=int(s[0]*ex_ratio)
+    y=int(s[1]*y_ratio)
+    driver.swipe(sx,y,ex,y,t)
