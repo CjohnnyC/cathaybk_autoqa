1b52aaf6cdfd14d2f602a35c154b62cbf2e67cd0
CjohnnyC <116575428+CjohnnyC@users.noreply.github.com>
2022-11-21 18:12:32 +0800

    Update testCathayApp.py
    Update android element out of screen issue

--- autotest/testCathayApp.py
+++ autotest/testCathayApp.py
@@ -16,9 +16,16 @@ class TestCathayApp:
     @classmethod
     def setup_class(cls):
         cls.driver=webdriver.Remote("http://localhost:4723/wd/hub", CAPS)
-        cls.driver.implicitly_wait(10) #We can also use WebDriverWait to each element, or set sleep.
+        
+        #We can also use WebDriverWait to each element, or set sleep.
+        cls.driver.implicitly_wait(10)
         cls.pe=PageElement(cls.driver)
         #cls.actions=TouchAction(cls.driver)
+
+        #Grouping elements prepared for eval(element)
+        cls.ccl=cls.pe.credit_card_list()
+        cls.cil=cls.pe.card_intro_list()
+        cls.dcg=cls.pe.dead_card_list()
 
     @classmethod
     def teardown_class(cls): 
@@ -32,59 +39,82 @@ class TestCathayApp:
 
     def test1_homepage(self):
         self.driver.get("https://www.cathaybk.com.tw/cathaybk/")
-        flag=True 
-        #implicity wait for page loading completed, then executing next script
-        #After page loading completed, then it executes flag=True
+         
+        #Page loading done in implicity wait, then executing next script, set flag=true
+        flag=True
+
+        #get screenshot with current time as file name
         NTIME=datetime.now().strftime(TIME_FORMAT)
         screenshot(self.driver, "./screenshot/homepage", "homepage_"+NTIME+".png")
-        assert flag==True #Confirm homepage displayed
+
+        #homepage should be loaded successfully
+        assert flag==True
+        
 
     def test2_credit_card_list(self):
         self.pe.menu().click()
         self.pe.prod_intro().click()
         self.pe.credit_card().click()
+        
+        #direct to credit card list, get screenshot of default output
         NTIME=datetime.now().strftime(TIME_FORMAT)
         screenshot(self.driver, "./screenshot/cardlist", "top_"+NTIME+".png")
         
-        ccl=self.pe.credit_card_list()
-        self.driver.execute_script("arguments[0].scrollIntoView(flase)", ccl[7])
-        #self.driver.scroll(ccl[i+1], ccl[i]) to scroll until show ccl[7]
-        #self.driver.drag_and_drop(ccl[i+1], ccl[i]) to scroll until show ccl[7]
-        #scroll_el1_to_el2(self.actions, ccl[7], ccl[0]) #TochAction not work on H5 page
+        #scroll to top by each credit card list element, until the target element displayed 
+        subcount=js_vertical_scroll_top(self.driver, self.ccl)
+        '''
+        self.driver.scroll(ccl[i+1], ccl[i]) to scroll until show ccl[7]
+        self.driver.drag_and_drop(ccl[i+1], ccl[i]) to scroll until show ccl[7]
+        scroll_el1_to_el2(self.actions, ccl[7], ccl[0]) #TochAction not work on H5 page
+        '''
+
+        all_sub_count=len(self.ccl)
         NTIME=datetime.now().strftime(TIME_FORMAT)
         screenshot(self.driver, "./screenshot/cardlist", "bottom_"+NTIME+".png")
-
-        self.driver.execute_script("arguments[0].scrollIntoView(true)", ccl[0])
-        # Pull back to ccl[0] (card_intro) for test3
+        #print(subcount)
         
-        assert len(ccl)==8 #Confirm credit card list has 8 subfunctions
+        #Confirm credit card list subfunction count is correct
+        assert subcount==all_sub_count 
 
     def test3_deadcard_type(self):
+        #pull back to card_intro
+        self.driver.execute_script("arguments[0].scrollIntoView(true)", self.pe.card_intro())
         self.pe.card_intro().click()
+
+        #horizontal scroll each card intro list element until dead card is displayed 
+        horizontal_scroll_each(self.driver, self.cil)
+        '''
         self.driver.scroll(self.pe.dptm_card(), self.pe.rcmd_card())
-        #default right border (dptm_card) position to scroll to left border until show dead_card
+        self.driver.scroll(self.pe.csld_card(), self.pe.dptm_card())
+        '''
+        
         self.pe.dead_card().click()
 
+        #to set overall screenshot and count
         screenshot_count=0
-        dcg=self.pe.dead_card_group()
-        card_x=dcg[0].location['x'] #first card x-coordinate
-        card_y=dcg[0].location['y'] #first card y-coordinate, and y will be fixed
-        card_width=dcg[0].size['width'] #first card width
-        for i in range(len(dcg)):
-            sleep(3)
+        eval(self.dcg[0])
+        all_cards_count=len(self.dcg)
+        for i in range(all_cards_count):
+            sleep(1) #To force seperate the time of getting screenshot
             NTIME=datetime.now().strftime(TIME_FORMAT)
             screenshot(self.driver, "./screenshot/cardgroup", str(i+1)+"_"+NTIME+".png")
             screenshot_count+=1
             
-            if i<len(dcg)-1:
-                swipe_card(self.driver, card_x, card_width, card_y)
-                #self.driver.scroll(dcg[i+1], dcg[i]) 
-                #concept is the same, but not sure the element out of screen
-                dcg[i+1] #To check next card displayed (by inplicity wait)
+            #swipe card by each one
+            if i < all_cards_count-1:
+                swipe_left_right(self.driver, 0.95, 0.05, 0.5)
+                '''
+                (sx=0.95) -> (ex=0.05) horizontal scroll screen from 95%->5% screen width
+                #(sy=0.5) anchor sy at 50% of screen height
+                #scroll_el1_to_el2(actions, dcg[i+1], dcg[i]) by TouchAction, but not sure the element out of screen
+                '''
+                
+                eval(self.dcg[i+1])
             else:
                 break
-        
-        assert screenshot_count==len(dcg) #Confirm screenshot num equal to dead card num
+
+        #Confirm screenshot num equal to dead card num
+        assert screenshot_count==all_cards_count
 
 if __name__=="__main__":
     NTIME=datetime.now().strftime(TIME_FORMAT)
