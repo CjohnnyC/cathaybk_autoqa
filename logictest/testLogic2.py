3a81d7929fc8f7bbb83c60b41fae2ed2bf22634e
CjohnnyC <116575428+CjohnnyC@users.noreply.github.com>
2022-11-22 11:24:21 +0800

    Update testLogic2.py

--- logictest/testLogic2.py
+++ logictest/testLogic2.py
@@ -1,17 +1,18 @@
 default_height=100
 rebound_times=10
+rebound_ratio=0.5
 
 def rebound_height(default_height, rebound_times):
-    return default_height*(0.5**rebound_times)
+    return default_height*(rebound_ratio**rebound_times)
 
-sum=0
+all_rebound=0
 for i in range(1, rebound_times+1):
-    sum+=rebound_height(default_height, i)
+    all_rebound+=rebound_height(default_height, i)
 
 tenth_rebound=rebound_height(default_height, rebound_times)
-all_traverse=sum*3-tenth_rebound
+all_traverse=all_rebound*(1+1/rebound_ratio)-tenth_rebound
 #all_rebound=sum
 
-print("所有落下和反彈高度總和為 "+str(all_traverse)+" 公分")
-print("第十次反彈高度為 "+str(tenth_rebound)+" 公分")
-#print("只計算共反彈十次的高度為 "+str(all_rebound)+" 公分")
+print("All traverse height(except 10th rebound) is "+str(all_traverse)+" cm")
+print("The 10th rebound height is "+str(tenth_rebound)+" cm")
+#print("Sum of 10 times rebound height is "+str(all_rebound)+" cm")
